1ff923401fffd96bce371804d755043b31764e9e
entiri <ntiri.e@gmail.com>
2021-03-26 13:22:01 -0400

    Generated unit tests for rescale_values_array

---
+++ ivadomed/test_rescale.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+from .maths import rescale_values_array
+
+
+# test rescale function
+
+@pytest.mark.parametrize("test_input, expected", [(np.ones((4, 4)), np.zeros((4, 4)))])
+def test_eval(test_input, expected):
+	assert np.sum(rescale_values_array(test_input)) == np.sum(expected)
+
