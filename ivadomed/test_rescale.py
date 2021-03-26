76b774934ba4624e05dc3cbfd26b3454eec7258c
entiri <ntiri.e@gmail.com>
2021-03-26 13:24:38 -0400

    test cases for branch

--- ivadomed/test_rescale.py
+++ ivadomed/test_rescale.py
@@ -4,7 +4,6 @@ from .maths import rescale_values_array
 
 
 # test rescale function
-
 @pytest.mark.parametrize("test_input, expected", [(np.ones((4, 4)), np.zeros((4, 4)))])
 def test_eval(test_input, expected):
 	assert np.sum(rescale_values_array(test_input)) == np.sum(expected)
